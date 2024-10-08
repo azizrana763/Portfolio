78e6797f983018f0d31199e80680d86bb4991ef6
azizrana763 <azizrana763@gmail.com>
2024-10-09 00:33:24 +0500

    resume updated finally

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -1,16 +1,16 @@
-import React, {useContext} from "react";
-import {Fade} from "react-reveal";
+import React, { useContext } from "react";
+import { Fade } from "react-reveal";
 import emoji from "react-easy-emoji";
 import "./Greeting.scss";
 import landingPerson from "../../assets/lottie/landingPerson";
 import DisplayLottie from "../../components/displayLottie/DisplayLottie";
 import SocialMedia from "../../components/socialMedia/SocialMedia";
 import Button from "../../components/button/Button";
-import {illustration, greeting} from "../../portfolio";
+import { illustration, greeting } from "../../portfolio";
 import StyleContext from "../../contexts/StyleContext";
 
 export default function Greeting() {
-  const {isDark} = useContext(StyleContext);
+  const { isDark } = useContext(StyleContext);
   if (!greeting.displayGreeting) {
     return null;
   }
@@ -41,9 +41,9 @@ export default function Greeting() {
                 <Button text="Contact me" href="#contact" />
                 {greeting.resumeLink && (
                   <a
-                    href={require("../../assets/resume.pdf")}
-                    download="Resume.pdf"
-                    className="download-link-button"
+                  href={greeting.resumeLink} // Use your drive PDF link here
+                  download="Resume.pdf"
+                  className="download-link-button"
                   >
                     <Button text="Download my resume" />
                   </a>
@@ -55,7 +55,7 @@ export default function Greeting() {
             {illustration.animated ? (
               <DisplayLottie animationData={landingPerson} />
             ) : (
-              <img 
+              <img
                 alt="man sitting on table"
                 src={require("../../assets/images/aziz.png")}
               ></img>
